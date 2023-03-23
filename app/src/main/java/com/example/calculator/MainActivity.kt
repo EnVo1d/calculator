ff8edffb366413fb986344ac0a3cbd5ad273a91e
Envoid <79973093+EnVo1d@users.noreply.github.com>
2023-03-23 10:33:23 +0200

    Dropdown menu

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,16 +3,49 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import kotlin.math.PI
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var currentItem: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.shapes)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.shapes_array,
+            android.R.layout.simple_spinner_item
+        ).also {
+            adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+            this.currentItem = adapter.getItem(0).toString()
+        }
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val item = parent.getItemAtPosition(pos)
+        when(currentItem) {
+            "Circle" -> {
+                findViewById<TextView>(R.id.param).text = "R ="
+            }
+            "Square" -> {
+                findViewById<TextView>(R.id.param).text = "a ="
+            }
+            else -> {
+                return
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        return
     }
 
     fun calculate(view: View) {
@@ -20,7 +53,8 @@ class MainActivity : AppCompatActivity() {
             ?: return;
         if(editTextValue == 0.0)
             return;
-        val res = ((PI * editTextValue.pow(2.0))*10).roundToInt() / 10.0;
-        findViewById<TextView>(R.id.result).text = res.toString();
+        val res = ((PI * editTextValue.pow(2.0))*10).roundToInt() / 10.0
+
+        findViewById<TextView>(R.id.result).text = res.toString()
     }
 }
